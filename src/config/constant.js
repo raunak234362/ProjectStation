e1d101b88d00e121da49ce40b16a8dd0c9630143
raunak234362 <raunakdob@gmail.com>
2024-11-06 12:20:33 +0530

    Refactor BASE_URL constant to use the remote API endpoint

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://192.168.1.49:8000/api/v2'
-// export const BASE_URL = 'https://projectstation.onrender.com/api/v2'
+// export const BASE_URL = 'http://192.168.1.49:8000/api/v2'
+export const BASE_URL = 'https://projectstation.onrender.com/api/v2'
 
 

7db4313235102437512c5dffafff7736ad5700db
raunak234362 <raunakdob@gmail.com>
2024-11-19 18:13:33 +0530

    Update Dashboard component to enhance project and vendor status display, add pie charts, and improve layout

--- src/components/dashboard/staff/admin/dashboard/Dashboard.jsx
+++ src/components/dashboard/staff/admin/dashboard/Dashboard.jsx
@@ -87,29 +87,31 @@ const Dashboard = () => {
       </div>
 
       <div className="h-[85vh] mt-2 overflow-y-auto">
-        <div className="my-5 grid md:grid-cols-2 grid-cols-1 gap-5">
-          <div className="grid text-center md:grid-cols-3 grid-cols-2 gap-5 ">
+        <div className="my-5 grid md:grid-cols-3 grid-cols-1 gap-5">
+
+
+          <div className="grid text-center md:grid-cols-2 grid-cols-2 gap-5 ">
             <div className="flex flex-col justify-center items-center bg-white/50 rounded-lg p-3 shadow-lg">
               <div className="font-bold text-base text-gray-800">
-                Total Projects
+                All Projects
               </div>
               <div className="text-3xl font-bold">{projects.length}</div>
             </div>
             <div className="flex flex-col justify-center items-center bg-white/50 rounded-lg p-3 shadow-lg">
               <div className="font-bold text-base text-gray-800">
-                Total Fabricators
+                All Fabricators
               </div>
               <div className="text-3xl font-bold">{completedProjectsCount}</div>
             </div>
             <div className="flex flex-col justify-center items-center bg-white/50 rounded-lg p-3 shadow-lg">
               <div className="font-bold text-base text-gray-800">
-                Total Clients
+                All Clients
               </div>
               <div className="text-3xl font-bold">{completedProjectsCount}</div>
             </div>
             <div className="flex flex-col justify-center items-center bg-white/50 rounded-lg p-3 shadow-lg">
               <div className="font-bold text-base text-gray-800">
-                Total Vendor
+                All Vendor
               </div>
               <div className="text-3xl font-bold">{completedProjectsCount}</div>
             </div>
@@ -126,109 +128,118 @@ const Dashboard = () => {
             </div>
           </div>
 
-          {/* Project Table */}
-          <div className="overflow-x-auto">
-            <div className="bg-white/70 rounded-lg md:w-full w-[90vw] p-4">
-              {/* Search and Filter Section */}
-              <div className="flex flex-col md:flex-row gap-4 mb-4">
-                <input
-                  type="text"
-                  placeholder="Search by project name..."
-                  value={searchTerm}
-                  onChange={handleSearch}
-                  className="px-4 py-2 border rounded-md w-full md:w-1/4"
-                />
-                <select
-                  value={statusFilter}
-                  onChange={handleStatusFilter}
-                  className="px-4 py-2 border rounded-md w-full md:w-1/4"
-                >
-                  <option value="">All Status</option>
-                  <option value="ASSIGNED">ASSIGNED</option>
-                  <option value="ACTIVE">ACTIVE</option>
-                  <option value="ON-HOLD">ON-HOLD</option>
-                  <option value="INACTIVE">INACTIVE</option>
-                  <option value="DELAY">DELAY</option>
-                  <option value="COMPLETED">COMPLETED</option>
-                </select>
-                <select
-                  value={fabricatorFilter}
-                  onChange={handleFabricatorFilter}
-                  className="px-4 py-2 border rounded-md w-full md:w-1/4"
-                >
-                  <option value="">All Fabricators</option>
-                  {uniqueFabricators.map((fabricator) => (
-                    <option key={fabricator} value={fabricator}>
-                      {fabricator}
-                    </option>
-                  ))}
-                </select>
-              </div>
+          {/* pie chart */}
+          <div className="bg-white/70 rounded-lg md:w-full w-[90vw] p-4">
+            WBT Project completion status
+            <img src="https://www.tableau.com/sites/default/files/2021-06/DataGlossary_Icons_Pie%20Chart.jpg" alt="" />
+          </div>
+          <div className="bg-white/70 rounded-lg md:w-full w-[90vw] p-4">
+            Vendor Project completion status
+            <img src="https://www.tableau.com/sites/default/files/2021-06/DataGlossary_Icons_Pie%20Chart.jpg" alt="" />
+          </div>
+        </div>
+        {/* Project Table */}
+        <div className="overflow-x-auto">
+          <div className="bg-white/70 rounded-lg md:w-full w-[90vw] p-4">
+            {/* Search and Filter Section */}
+            <div className="flex flex-col md:flex-row gap-4 mb-4">
+              <input
+                type="text"
+                placeholder="Search by project name..."
+                value={searchTerm}
+                onChange={handleSearch}
+                className="px-4 py-2 border rounded-md w-full md:w-1/4"
+              />
+              <select
+                value={statusFilter}
+                onChange={handleStatusFilter}
+                className="px-4 py-2 border rounded-md w-full md:w-1/4"
+              >
+                <option value="">All Status</option>
+                <option value="ASSIGNED">ASSIGNED</option>
+                <option value="ACTIVE">ACTIVE</option>
+                <option value="ON-HOLD">ON-HOLD</option>
+                <option value="INACTIVE">INACTIVE</option>
+                <option value="DELAY">DELAY</option>
+                <option value="COMPLETED">COMPLETED</option>
+              </select>
+              <select
+                value={fabricatorFilter}
+                onChange={handleFabricatorFilter}
+                className="px-4 py-2 border rounded-md w-full md:w-1/4"
+              >
+                <option value="">All Fabricators</option>
+                {uniqueFabricators.map((fabricator) => (
+                  <option key={fabricator} value={fabricator}>
+                    {fabricator}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-              {/* Project Table */}
-              <div className="mt-5 bg-white h-[30vh] overflow-y-auto rounded-lg">
-                <table className=" md:w-full overflow-auto w-[90vw] border-collapse text-center md:text-lg text-xs rounded-xl">
-                  <thead>
-                    <tr className="bg-teal-200/70">
-                      <th
-                        className="px-2 py-1 text-left cursor-pointer"
-                        onClick={() => handleSort("name")}
-                      >
-                        Project Name{" "}
-                        {sortConfig.key === "name" &&
-                          (sortConfig.direction === "ascending" ? "▲" : "▼")}
-                      </th>
+            {/* Project Table */}
+            <div className="mt-5 bg-white h-[30vh] overflow-y-auto rounded-lg">
+              <table className=" md:w-full overflow-auto w-[90vw] border-collapse text-center md:text-lg text-xs rounded-xl">
+                <thead>
+                  <tr className="bg-teal-200/70">
+                    <th
+                      className="px-2 py-1 text-left cursor-pointer"
+                      onClick={() => handleSort("name")}
+                    >
+                      Project Name{" "}
+                      {sortConfig.key === "name" &&
+                        (sortConfig.direction === "ascending" ? "▲" : "▼")}
+                    </th>
 
-                      <th
-                        className="px-2 py-1 cursor-pointer"
-                        onClick={() => handleSort("status")}
-                      >
-                        Project Status{" "}
-                        {sortConfig.key === "status" &&
-                          (sortConfig.direction === "ascending" ? "▲" : "▼")}
-                      </th>
+                    <th
+                      className="px-2 py-1 cursor-pointer"
+                      onClick={() => handleSort("status")}
+                    >
+                      Project Status{" "}
+                      {sortConfig.key === "status" &&
+                        (sortConfig.direction === "ascending" ? "▲" : "▼")}
+                    </th>
 
-                      <th className="px-2 py-1">Actions</th>
+                    <th className="px-2 py-1">Actions</th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {filteredProjects.length === 0 ? (
+                    <tr className="bg-white">
+                      <td colSpan="6" className="text-center">
+                        No Projects Found
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody className="">
-                    {filteredProjects.length === 0 ? (
-                      <tr className="bg-white">
-                        <td colSpan="6" className="text-center">
-                          No Projects Found
+                  ) : (
+                    filteredProjects.map((project, index) => (
+                      <tr
+                        key={project.id}
+                        className="hover:bg-blue-gray-100 border"
+                      >
+                        <td className="border px-2 py-1 text-left">
+                          {project.name}
+                        </td>
+
+                        <td className="border px-2 py-1">{project.status}</td>
+
+                        <td className="border px-2 py-1">
+                          <Button onClick={() => handleViewClick(project.id)}>
+                            View
+                          </Button>
                         </td>
                       </tr>
-                    ) : (
-                      filteredProjects.map((project, index) => (
-                        <tr
-                          key={project.id}
-                          className="hover:bg-blue-gray-100 border"
-                        >
-                          <td className="border px-2 py-1 text-left">
-                            {project.name}
-                          </td>
-                          
-                          <td className="border px-2 py-1">{project.status}</td>
-                          
-                          <td className="border px-2 py-1">
-                            <Button onClick={() => handleViewClick(project.id)}>
-                              View
-                            </Button>
-                          </td>
-                        </tr>
-                      ))
-                    )}
-                  </tbody>
-                </table>
-              </div>
-              {selectedProject && (
-                <GetProject
-                  projectId={selectedProject}
-                  isOpen={isModalOpen}
-                  onClose={handleModalClose}
-                />
-              )}
+                    ))
+                  )}
+                </tbody>
+              </table>
             </div>
+            {selectedProject && (
+              <GetProject
+                projectId={selectedProject}
+                isOpen={isModalOpen}
+                onClose={handleModalClose}
+              />
+            )}
           </div>
         </div>
       </div>

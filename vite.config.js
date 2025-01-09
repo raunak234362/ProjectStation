09adb5c9d4be70acd80bd939717949191851eb54
raunak234362 <raunakdob@gmail.com>
2025-01-09 13:48:57 +0530

    Update API proxy target to use HTTPS in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://192.168.1.153:8000/api',
+        target: 'https://192.168.1.153:8000/api',
         changeOrigin: true,
         secure: false, // For development only
       },

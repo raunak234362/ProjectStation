7e971193b90737a46a25b570a80f4bd7e271a42a
raunak234362 <raunakdob@gmail.com>
2025-01-21 13:57:34 +0530

    Update GetFabricator component width for improved responsiveness

--- src/components/dashboard/staff/admin/fabricator/GetFabricator.jsx
+++ src/components/dashboard/staff/admin/fabricator/GetFabricator.jsx
@@ -76,7 +76,7 @@ const GetFabricator = ({ fabricatorId, isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white h-[80%] md:p-5 p-2 rounded-lg shadow-lg w-6/12 ">
+      <div className="bg-white h-[80%] md:p-5 p-2 rounded-lg shadow-lg w-11/12 md:w-6/12 ">
         <div className="flex flex-row justify-between">
           <Button className="bg-red-500" onClick={handleClose}>
             Close

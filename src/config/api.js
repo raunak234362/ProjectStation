4cb1a900995f750ee7bd7bbae44d83b480c58d83
raunak234362 <raunakdob@gmail.com>
2025-01-21 19:39:16 +0530

    Update API configuration to include HTTPS agent settings for improved security handling

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://106.51.141.125:5154',
+  httpsAgent: {
+    rejectUnauthorized: false
+  }
 });
 
 export default instance;

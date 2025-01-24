ddcf51a2f1bd8609914741af2afa9e41d982aea8
raunak234362 <raunakdob@gmail.com>
2025-01-24 11:32:13 +0530

    Update API base URL to local development endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://106.51.141.125:5153',
-  // baseURL: 'http://192.168.1.153:5153',
-  baseURL: 'https://projectstationbe.onrender.com/',
+  baseURL: 'https://192.168.1.153:5154',
+  // baseURL: 'https://projectstationbe.onrender.com/',
   httpsAgent: {
     rejectUnauthorized: false
   }
